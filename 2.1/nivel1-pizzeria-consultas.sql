a3adb110f2ba7ecb5832dcc182db20193555352a
SergiFont <sfontsmx2010@gmail.com>
2023-01-30 20:49:44 +0100

    Revisadas consultas de la pizzería

--- 2.1/nivel1-pizzeria-consultas.sql
+++ 2.1/nivel1-pizzeria-consultas.sql
@@ -1,23 +1,3 @@
--- Pizzeria:
--- Llista quants productes de tipus “Begudes”. s'han venut en una determinada localitat.
-
-USE pizzeriaSergi;
-
-SELECT c.city as 'Localidad', sum(od.quantity) as 'Cantidad de bebidas vendidas'
-FROM costumers c
-JOIN orders o
-	on c.costumer_id=o.costumer_id
-JOIN order_items oi
-	on o.order_id=oi.order_id
-JOIN order_drink od
-	on oi.order_id=od.order_id
-WHERE c.city = 'Barcelona';
-
--- Llista quantes comandes ha efectuat un determinat empleat/da.
-
-SELECT concat(e.first_name,' ', e.surname) as 'Employee', count(o.order_id) as 'Orders'
-FROM employees e
-JOIN orders o
-	on o.employee_id=e.employee_id
-WHERE e.employee_id=3
+USE pizzeriaSergi; SELECT c.city as 'Localidad', sum(oi.quantity) as 'Cantidad de bebidas vendidas' FROM costumers c JOIN orders o on c.costumer_id=o.costumer_id JOIN order_items oi on o.order_id=oi.order_id WHERE c.city = 'Barcelona' AND oi.product_id>=16;
+SELECT concat(e.first_name,' ', e.surname) as 'Employee', count(o.order_id) as 'Orders' FROM employees e JOIN orders o on o.employee_id=e.employee_id WHERE e.employee_id=3;
 
